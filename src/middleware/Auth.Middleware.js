ed9ca2496982b969e1dc504175ac021838379ae9
Vivek-Hello <vickyvivek391@gmail.com>
2025-05-01 22:46:59 +0530

    adding all the files

--- src/middleware/Auth.Middleware.js
+++ src/middleware/Auth.Middleware.js
@@ -7,11 +7,12 @@ export const generateToken = (id, res) => {
         const token = jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: "7d" });
 
         res.cookie("token", token, {
-            maxAge: 7 * 24 * 60 * 60 * 1000, // Matches "7d"
+            maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: "strict",
-            secure: process.env.NODE_ENV === "production", // Secure in production only
-        });
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+            secure: process.env.NODE_ENV === "production",
+          });
+          
 
         return token;
     } catch (error) {
